95c7ff64ee9b1534a636eed0b7f1bdcc5d945fc9
Brian <wamwangimathenge@gmail.com>
2023-09-02 12:27:46 +0300

    Write a loop that iterates over a collection

---
+++ loop.py
@@ -0,0 +1,16 @@
+## For Loop
+
+## Looping through a collection
+
+for name in ["Christopher", "Susan"]:
+    print(name)
+    
+    
+for tool in ["Python", "Linux", "Terraform", "Docker", "AWS"]:
+    print(tool)
+    
+    
+backend_languages = ["JS", "Ruby", "Python", "Go"]
+
+for lang in backend_languages:
+    print(lang)
